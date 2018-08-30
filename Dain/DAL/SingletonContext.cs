1a901624ad72b831fdb5ef630e61cd130df02083
joaoggs <joaogabrielggs@gmail.com>
2018-08-30 16:03:48 -0300

    Context and SingletonContext / DAL

---
+++ Dain/DAL/SingletonContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dain.Models;
+
+namespace Dain.DAL
+{
+    public class SingletonContext
+    {
+        private static Context ctx;
+        private SingletonContext() { }
+
+        public static Context GetInstance()
+        {
+            if(ctx == null)
+            {
+                ctx = new Context();
+            }
+            return ctx;
+        }
+    }
+}
---
+++ Dain/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Dain.Models
+{
+    public class Context : DbContext
+    {
+        public Context() : base("DbDain") { }
+
+        //public DbSet<Pub> Pubs { get; set; }
+        //public DbSet<Person> Persons { get; set; }
+    }
+}
