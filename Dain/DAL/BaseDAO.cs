0f71aad4f94bc77e240327a9950d56fcde43bfe2
joaoggs <joaogabrielggs@gmail.com>
2018-09-17 23:37:55 -0300

    DAL - BaseDAO Insert / Update / Delete / Search

---
+++ Dain/DAL/BaseDAO.cs
@@ -0,0 +1,62 @@
+﻿using Dain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Dain.DAL
+{
+    public class BaseDAO<T> where T : User
+    {
+        private static Context db = SingletonContext.GetInstance();
+        public static bool Insert(T entidade)
+        {
+            try
+            {
+                db.Set<T>().Add(entidade);
+                db.SaveChanges();
+                return true;
+            }
+            catch { return false; }
+
+        }
+
+        public static bool Update(T entidade)
+        {
+            try
+            {
+                db.Set<T>().Attach(entidade);
+                db.Entry<T>(entidade).State = EntityState.Modified;
+                db.SaveChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public static void Delete(T entidade)
+        {
+            db.Entry<T>(entidade).State = EntityState.Deleted;
+            db.Set<T>().Remove(entidade);
+
+            db.SaveChanges();
+        }
+
+        public static T Search(int? id)
+        {
+            int Id = int.Parse(id.ToString());
+            return db.Set<T>().Find(id);
+        }
+
+        public static T Search(string Email, string Password)
+        {
+            try
+            {
+                return db.Set<T>().FirstOrDefault(x => x.Email.Equals(Email) && x.Password.Equals(Password));
+            }
+            catch
+            { return null; }
+        }
+
+    }
+}
---
+++ Dain/DAL/PersonDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dain.Models;
+
+namespace Dain.DAL
+{
+    public class PersonDAO : BaseDAO<Person>
+    {
+    }
+}
