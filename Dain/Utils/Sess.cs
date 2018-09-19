87c9ee343d7b7ca3089774bafb35688f26c4011e
joaoggs <joaogabrielggs@gmail.com>
2018-09-18 21:31:47 -0300

    Utils - Session ReturnId / Clear

---
+++ Dain/Utils/Sess.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dain.Utils
+{
+    public class Sess
+    {
+        public static int ReturnPubId(int? id)
+        {
+            if (HttpContext.Current.Session["PubID"] == null)
+            {
+                HttpContext.Current.Session["PubID"] = (id);
+            }
+
+            return int.Parse(HttpContext.Current.Session["PubID"].ToString());
+        }
+
+        public static int ReturnPersonId(int? id)
+        {
+            if (HttpContext.Current.Session["PersonID"] == null)
+            {
+                HttpContext.Current.Session["PersonID"] = (id);
+            }
+
+            return int.Parse(HttpContext.Current.Session["PersonID"].ToString());
+        }
+
+        public static void ClearPubSession()
+        {
+            HttpContext.Current.Session["UserID"] = null;
+        }
+
+        public static void ClearPersonSession()
+        {
+            HttpContext.Current.Session["PersonID"] = null;
+        }
+    }
+}
