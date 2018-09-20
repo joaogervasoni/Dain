081dc66cea7de1f8195b21d4c9ea5076eea2bf1e
joaoggs <joaogabrielggs@gmail.com>
2018-09-20 19:30:26 -0300

    Views - Image: Save / Update

--- Dain/Scripts/Dashboard.js
+++ Dain/Scripts/Dashboard.js
@@ -31,3 +31,12 @@
         $(name).css("display", "initial");
     }
 }
+
+function Preview_image(event) {
+    var reader = new FileReader();
+    reader.onload = function () {
+        var output = document.getElementById('output_image');
+        output.src = reader.result;
+    }
+    reader.readAsDataURL(event.target.files[0]);
+}
--- Dain/Scripts/Login.js
+++ Dain/Scripts/Login.js
@@ -7,3 +7,12 @@ function LoginJS() {
     $("#loginForm").css("display", "initial");
     $("#registerForm").css("display", "none");
 }
+
+function Preview_image(event) {
+    var reader = new FileReader();
+    reader.onload = function () {
+        var output = document.getElementById('output_image');
+        output.src = reader.result;
+    }
+    reader.readAsDataURL(event.target.files[0]);
+}
