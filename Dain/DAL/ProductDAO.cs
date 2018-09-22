82609b1b0145cbce578460648ebeeb0e0b77875d
joaoggs <joaogabrielggs@gmail.com>
2018-09-21 22:44:55 -0300

    DAL - ProductDAO: Insert / Update / Search / ReturnList

---
+++ Dain/DAL/ProductDAO.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Dain.Models;
+
+namespace Dain.DAL
+{
+    public class ProductDAO
+    {
+        private static Context db = SingletonContext.GetInstance();
+
+        public static bool Insert(Product product)
+        {
+            try
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public static bool Delete(int id)
+        {
+            try
+            {
+                var product = Search(id);
+                db.Entry(product).State = EntityState.Deleted;
+                db.Products.Remove(product);
+
+                db.SaveChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public static Product Search(int id)
+        {
+            try
+            {
+                return db.Products.Find(id);
+            }
+            catch { return null; }
+        }
+
+        public static List<Product> ReturnList()
+        {
+            try
+            {
+                return db.Products.ToList();
+            }
+            catch { return null; }
+        }
+
+        public static List<Product> ReturnList(int PubId)
+        {
+            try
+            {
+                return db.Products.Where(x => x.PubId == PubId).ToList();
+            }
+            catch { return null; }
+        }
+    }
+}
